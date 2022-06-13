16d4ad136a9508df97983c7f06678053a4bc9419
Ricky <ochor01@pfw.edu>
2022-06-13 14:15:51 -0400

    First commit -Complete ejs  template

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+  
+    let today = new Date();
+    let currentDay = today.getDay();
+    let day = "";
+
+    if(currentDay === 6 || currentDay ==0){
+        day = "weekend";
+       
+      
+    }else{
+        day = "weekday";
+      
+    }
+
+    res.render('list', {kindOfDay:day})
+
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
